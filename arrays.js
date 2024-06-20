26a71c78c4995196fb6a256f7dda747b1d239f79
vinayjoshi434 <vinayjoshi434@gmail.com>
2024-06-20 18:25:57 +0530

    add arrays basics

---
+++ arrays.js
@@ -0,0 +1,35 @@
+// const arr=Array("red","white","black");
+// console.log(arr);
+// console.log(typeof arr)
+// const arr=[1,2,3,4]
+// console.log(typeof arr);
+
+// const arr1=Array(3);
+// console.log(arr1);
+// for(item of arr1){
+//     console.log(item)
+// }
+
+// const arr3=new Array(4)
+// console.log(arr3);
+
+
+//investigate the shallow copy 
+
+// let brands=["tata","mahindra","toyota","kia"]
+// const commercialvechiclebrand=brands
+
+// console.log(commercialvechiclebrand);
+
+// commercialvechiclebrand.push("ashokaleyland","Eicher","bharatBenz","MAN")
+
+// console.log(brands);                                   // here changes performed in the orignal brands array also
+
+// Array methods
+
+const arr=[1,3,4,5,6,]
+const newarr=arr.join(",")
+console.log(typeof newarr);
+console.log(newarr);
+
+
