7fb2049932f3718199e7468dfe33fb4e970ec529
vinayjoshi434 <vinayjoshi434@gmail.com>
2024-06-21 11:25:57 +0530

    "intro to datatypesconversion"

---
+++ datatypeconversion.js
@@ -0,0 +1,21 @@
+// const name=""
+// const bool=Boolean(name)
+// console.log(typeof bool);
+// console.log(bool);
+
+
+// let num=new Number( );
+// console.log(num);
+// console.log(typeof num);
+
+// let num=23;
+// console.log(num);
+
+let num1=Number(23);
+console.log(num1)
+
+// let num2=new Number("43")
+// console.log(num2)
+// // Number()
+// let tc=Number("andn")
+// console.log(tc);
